5e4025dd18f20de3d8513ff6ec34e62ef0d8a4e7
eldhosekurianmathewofficial <eldhosekurianmathewofficial@gmail.com>
2024-02-08 15:34:29 +0530

    viewdataincluded

--- controllers/patientdata.js
+++ controllers/patientdata.js
@@ -2,12 +2,21 @@ const express = require("express")
 const router = express.Router()
 const patientModel = require("../models/patientModel")
 
+
+
 router.post("/add",async(req,res)=>{
     let data=req.body
     let mod = new patientModel(data)
     let result=await mod.save()
     res.json({
         status:"success"
+    })
+})
+
+router.get("/view",async(req,res)=>{
+    let dat=await patientModel.find()
+    res.json({
+        dat
     })
 })
 module.exports = router
